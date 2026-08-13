f10c61c0850d4ffd51b2d18fa9bf922335c8dd58
Rahat <rrahatislamm@gmail.com>
2026-08-13 21:17:11 +0530

    numpy slicing done!!

---
+++ NumPy/slicing.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+array = np.array([[1, 2, 3, 4], 
+                  [5, 6, 7, 8], 
+                  [9, 10, 11, 12], 
+                  [13, 14, 15, 16]])
+
+# array[start: end : step]
+
+# print(array[1])
+# print(array[-1])
+
+# print(array[0 : 3])
+# print(array[0:])
+# print(array[:3])
+
+# print(array[0:4:2])
+
+# print(array[0, 0])
+# print(array[:, 1])
+
+# print(array[0:2, -1])
+
+# print(array[0:2, 1:3])
+
+# print(array[:, ::2])
+
+# print(array[:, 1::2])
+
+# print(array[:, ::-2])
+
+
+# print(array[0: 2, 0: 2])
+
+
+
